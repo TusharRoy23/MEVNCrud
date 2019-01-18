7a65d6dd8f3d5b53191f1510a1e7f66a83836e41
Tushar Roy Chowdhury <risenboy23@gmail.com>
2019-01-18 21:04:52 +0600

    Update Api.js

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: "http://192.168.0.126:4040"
+        baseURL: "http://localhost:4040" // Use your port
     })
 }
